7a54d9c92fff85ef53b06691625e6539bb97711a
OlekZima <zimenkoalex34@gmail.com>
2026-02-25 20:02:28 +0100

    Move Answer dataclass to the question.py and implement Gakko question validation

--- src/svg2gakko/question.py
+++ src/svg2gakko/question.py
@@ -1,5 +1,5 @@
+from cairocffi import Error
 from dataclasses import dataclass
-from svg2gakko.answer import Answer
 from enum import Enum
 
 
@@ -10,6 +10,20 @@ class QuestionType(Enum):
 
 
 @dataclass
+class Answer:
+    content: str
+    correct: bool
+    weight: int = 1
+
+    def to_dict(self):
+        return {
+            "Content": self.content,
+            "Correct": self.correct,
+            "Weight": self.weight,
+        }
+
+
+@dataclass
 class Question:
     content: str
     question_type: QuestionType
@@ -17,7 +31,20 @@ class Question:
     answers: list[Answer]
     points: int = 1
 
+    def validate(self):
+        if self.question_type.value in (0, 1):
+            if len(self.answers) < 2:
+                raise ValueError("For MULTIPLE/SINGLE_CHOICE_QUESTION you need at least 2 answers.")
+            if not any(answer.correct for answer in self.answers):
+                raise ValueError(
+                    "For MULTIPLE/SINGLE_CHOICE_QUESTION at least one answer should be correct."
+                )
+
+        if not all(answer.correct for answer in self.answers):
+            raise ValueError("For TEXT_QUESTION all of the answers should be correct.")
+
     def to_dict(self):
+        self.validate()
         return {
             "Content": self.content,
             "NumberOfOptions": self.number_of_options,
@@ -25,15 +52,3 @@ class Question:
             "Points": self.points,
             "Answers": [answer.to_dict() for answer in self.answers],
         }
-
-    # def __init__(
-    #     self,
-    #     content: str,
-    #     question_type: QuestionType,
-    #     number_of_options: int,
-    #     answers: list[Answer],
-    # ):
-    #     self.content = content
-    #     self.question_type = question_type
-    #     self.number_of_options = 3 if number_of_options < 3 else number_of_options
-    #     self.answers = answers
