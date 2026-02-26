a925665d55133ba8da4b8f78359812323a4f064b
OlekZima <zimenkoalex34@gmail.com>
2026-02-26 17:48:42 +0100

    Refactor Question initialization in test cases to remove unnecessary parameter

--- tests/test_dataclass.py
+++ tests/test_dataclass.py
@@ -9,56 +9,56 @@ import pytest
 
 def test_validation_single_choice_raise():
     answers = [Answer("Answer 1", False), Answer("Answer 2", False)]
-    question = Question("Question 1", QuestionType.SINGLE_CHOICE_QUESTION, 4, answers)
+    question = Question("Question 1", QuestionType.SINGLE_CHOICE_QUESTION, answers)
     with pytest.raises(NotAtLeastOneCorrectAnswersError):
         question.to_dict()
 
 
 def test_validation_single_choice():
     answers = [Answer("Answer 1", True), Answer("Answer 2", False)]
-    question = Question("Question 1", QuestionType.SINGLE_CHOICE_QUESTION, 4, answers)
+    question = Question("Question 1", QuestionType.SINGLE_CHOICE_QUESTION, answers)
 
     question.to_dict()
 
 
 def test_validation_multiple_choice_raise():
     answers = [Answer("Answer 1", False), Answer("Answer 2", False)]
-    question = Question("Question 1", QuestionType.MULTIPLE_CHOICE_QUESTION, 4, answers)
+    question = Question("Question 1", QuestionType.MULTIPLE_CHOICE_QUESTION, answers)
     with pytest.raises(NotAtLeastOneCorrectAnswersError):
         question.to_dict()
 
 
 def test_validation_multiple_choice():
     answers = [Answer("Answer 1", True), Answer("Answer 2", False)]
-    question = Question("Question 1", QuestionType.MULTIPLE_CHOICE_QUESTION, 4, answers)
+    question = Question("Question 1", QuestionType.MULTIPLE_CHOICE_QUESTION, answers)
 
     question.to_dict()
 
 
 def test_validation_single_no_answers():
     answers = []
-    question = Question("Question 1", QuestionType.SINGLE_CHOICE_QUESTION, 4, answers)
+    question = Question("Question 1", QuestionType.SINGLE_CHOICE_QUESTION, answers)
     with pytest.raises(NotAtLeastTwoAnswersError):
         question.to_dict()
 
 
 def test_validation_multiple_no_answers():
     answers = []
-    question = Question("Question 1", QuestionType.MULTIPLE_CHOICE_QUESTION, 4, answers)
+    question = Question("Question 1", QuestionType.MULTIPLE_CHOICE_QUESTION, answers)
     with pytest.raises(NotAtLeastTwoAnswersError):
         question.to_dict()
 
 
 def test_validation_text_no_answers():
     answers = []
-    question = Question("Question 1", QuestionType.TEXT_QUESTION, 4, answers)
+    question = Question("Question 1", QuestionType.TEXT_QUESTION, answers)
 
     question.to_dict()
 
 
 def test_validation_text_false_answers():
     answers = [Answer("Answer 1", True), Answer("Answer 2", False)]
-    question = Question("Question 1", QuestionType.TEXT_QUESTION, 4, answers)
+    question = Question("Question 1", QuestionType.TEXT_QUESTION, answers)
 
     with pytest.raises(NotAllAnswerCorrectError):
         question.to_dict()
